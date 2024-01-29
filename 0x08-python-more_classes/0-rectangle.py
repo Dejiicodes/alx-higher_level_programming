0665ddef56d8fd78a765e7e22a9385701945986d
Dejiicodes <adewuniayodeji17@gmail.com>
2024-01-29 03:43:28 -0800

    2nd commit for 0x08-python-more_classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Empty class Rectangle that defines a rectangle."""
+
+
 class Rectangle:
-    """ Empty class """
+    """Empty class Rectangle that defines a rectangle."""
     pass
