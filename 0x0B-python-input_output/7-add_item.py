5377167fdb26cbff80aeea52a49520b103a55684
Dejiicodes <adewuniayodeji17@gmail.com>
2024-02-06 11:50:40 -0800

    5th commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,22 +4,22 @@ Script that adds all arguments to a Python list, and then save them to a file.
 """
 
 import sys
+import json
 import os.path
 
 args = sys.argv[1:]
 
 if not os.path.exists("add_item.json"):
     with open("add_item.json", "w") as file:
-        file.write("[]")
+        json.dump([], file)
 
 with open("add_item.json", "r") as file:
-    data = file.read()
-    if data:
-        items = eval(data)
-    else:
-        items = []
+    items = json.load(file)
+
+if not isinstance(items, list):
+    items = []
 
 items.extend(args)
 
 with open("add_item.json", "w") as file:
-    file.write(str(items))
+    json.dump(items, file)
