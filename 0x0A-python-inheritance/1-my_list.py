1b42a58b478d298af32e8e0a32d9f45a545422af
Dejiicodes <adewuniayodeji17@gmail.com>
2024-02-06 08:58:59 -0800

    Trying again

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 
+"""
+This module defines a class MyList that inherits from list.
+"""
+
+
 class MyList(list):
     """Custom list class."""
 
     def print_sorted(self):
-        """Prints the sorted list."""
+        """Prints the list in sorted order."""
         print(sorted(self))
 
 # Test cases
-my_list = MyList()
-my_list.append(4)
-my_list.append(2)
-my_list.append(1)
-my_list.append(3)
-my_list.print_sorted()
+if __name__ == "__main__":
+    ml = MyList()
+    ml.append(1)
+    ml.append(3)
+    ml.append(2)
+    ml.append(-1)
+    ml.append(-5)
+    ml.append(-3)
+    ml.append(0)
+    ml.print_sorted()
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 
+"""
+This module defines a function is_same_class.
+"""
+
+
 def is_same_class(obj, a_class):
-    """Checks if an object is an instance of a specific class."""
-    return type(obj) is a_class
+    """Checks if an object is an instance of a given class."""
+    return type(obj) == a_class
 
 # Test cases
-a = 1
-print(is_same_class(a, int))  # True
-print(is_same_class(a, float))  # False
+if __name__ == "__main__":
+    a = 1
+    print(is_same_class(a, int))
+    a = True
+    print(is_same_class(a, int))
+    a = 3.14
+    print(is_same_class(a, int))
+    a = True
+    print(is_same_class(a, object))
+    a = None
+    print(is_same_class(a, object))
+    a = None
+    print(is_same_class(a, list))
+    a = [1, 2, 3]
+    print(is_same_class(a, list))
+    a = [1, 2, 3]
+    print(is_same_class(a, object))
