c51649d4aa8520dd4305340b1198fbe9bb031d5c
Dejiicodes <adewuniayodeji17@gmail.com>
2024-02-06 12:42:18 -0800

    8th commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,52 +1,20 @@
 #!/usr/bin/python3
-"""
-This module defines a function to load, add, and save to a JSON file.
-"""
+"""Defines a Student class."""
 
-import json
 
-def load_from_json_file(filename):
-    """
-    Creates a Python object from a JSON file.
+class Student:
+    """Represents a student."""
 
-    Args:
-        filename (str): The name of the JSON file to load from.
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    Returns:
-        object: Python object loaded from the JSON file.
-    """
-    with open(filename, mode='r', encoding='utf-8') as file:
-        return json.load(file)
-
-def save_to_json_file(my_obj, filename):
-    """
-    Saves a Python object to a file in JSON format.
-
-    Args:
-        my_obj: The Python object to save.
-        filename (str): The name of the file to save to.
-    """
-    with open(filename, mode='w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
-
-def load_add_save(filename, key, value):
-    """
-    Loads a JSON file, adds a new key-value pair, and saves the updated JSON to the file.
-
-    Args:
-        filename (str): The name of the JSON file to load from and save to.
-        key: The key to add to the JSON.
-        value: The value associated with the key.
-    """
-    try:
-        data = load_from_json_file(filename)
-    except FileNotFoundError:
-        data = {}
-    data[key] = value
-    save_to_json_file(data, filename)
-
-if __name__ == "__main__":
-    filename = "add_item_advanced.json"
-    key = "name"
-    value = "John"
-    load_add_save(filename, key, value)
+    def to_json(self):
+        """Retrieve a dictionary representation of a Student instance."""
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
