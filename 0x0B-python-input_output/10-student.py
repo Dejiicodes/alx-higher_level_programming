bc77d43a50aefad013aec0e183776e02ec366965
Dejiicodes <adewuniayodeji17@gmail.com>
2024-02-06 12:45:52 -0800

    9th commit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python3
-"""
-This module defines a function to convert a class instance to a JSON string.
-"""
+"""Module for Student class"""
 
-import json
 
 class Student:
-    """
-    Represents a student with first name, last name, and age.
-    """
+    """Defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes a Student instance with the given attributes.
-        """
+        """Initializes a Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """
-        Converts the student object to a JSON dictionary.
-
-        Returns:
-            dict: JSON representation of the student object.
-        """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
 
 if __name__ == "__main__":
     student_1 = Student("John", "Doe", 23)
-    print(json.dumps(student_1.to_json()))
+    student_2 = Student("Bob", "Dylan", 27)
+
+    j_student_1 = student_1.to_json()
+    j_student_2 = student_2.to_json(['first_name', 'age'])
+    j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+    print(j_student_1)
+    print(j_student_2)
+    print(j_student_3)
